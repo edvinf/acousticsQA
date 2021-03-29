5fd98d43b362e5546f5b1c770d895d6559db0e9b
edvinf <edvin.fuglebakk@gmail.com>
2021-03-29 22:32:53 +0200

    init for acoustics QA

---
+++ R/acoustics.R
@@ -0,0 +1,307 @@
+library(RstoxData)
+library(ggplot2)
+
+
+#' Horisontal Profile
+#'
+#' @description
+#'  sa in horisontal bands of the water column, along with their distance from seadbed (bottom)
+#'
+#' @details
+#'  data table with columns:
+#'  \describe{
+#'   \item{height}{Factor for each horisontal band. Names indicate interval in meters.}
+#'   \item{sa}{Aggregated sa in horsintal band}
+#'  }
+#' @name horisontalProfile
+NULL
+
+
+#' Extract Horisontal profile
+#' @description
+#'  Makes horisontal profile (sa by approximate distance from bottom) from pelagic channels
+#' @details
+#'  Consult reference table for acoustic categories.
+#'  https://referenceeditor.hi.no/apps/referenceeditor/v2/tables/acousticCategory
+#'
+#'  some common options:
+#'  1 (other)
+#'  2
+#'  6
+#'  12 (herring)
+#'  22 (saithe)
+#'  30 (haddock)
+#'  31 (cod)
+#'
+#' @param echosounder luf20 as read by RstoxData::readXmlFile
+#' @param targetAcocat acoustic category to extract profile for
+#' @param bins number of bins for the profile
+#' @param freq frequncy of transreceiver to extract data from.
+#' @return \code{\link[acousticsQA]{horisontalProfile}}
+#' @export
+horisontalProfileLUF20 <- function(echosounder, targetAcocat, bins=20, freq=38000){
+  target <- echosounder$sa_by_acocat[echosounder$sa_by_acocat$acocat==targetAcocat,]
+  targetPC <- target[target$type=="P",]
+  targetPC38 <- targetPC[targetPC$freq==freq,]
+  targetPC38A <- merge(targetPC38, echosounder$frequency, all.x=T)
+  targetSA <- merge(targetPC38A, echosounder$sa, all.x=T, by=c("log_start", "start_time", "freq", "transceiver", "type", "acocat"))
+  targetSA <- merge(targetSA, echosounder$distance, all.x=T, by=c("log_start", "start_time"))
+  stopifnot(length(unique(targetSA$transceiver))==1)
+  targetSA$height <- targetSA$max_bot_depth - targetSA$ch*targetSA$pel_ch_thickness
+  targetSA$heightbins <- cut(targetSA$height, breaks=bins, ordered_result = T)
+  profile <- aggregate(list(sa=targetSA$sa), by=list(height=targetSA$heightbins), FUN=sum)
+
+  return(profile)
+}
+
+#' Vertical Profile
+#'
+#' @description
+#'  sa in vertical bands of the water column, along with log (sailed distance),
+#'  position parameters and time parameters
+#'
+#' @details
+#'  data table with columns:
+#'  \describe{
+#'   \item{sa}{Aggregated sa in vertical band}
+#'   \item{log}{log, sailed distance in nmi}
+#'   \item{time}{time as POSIXct}
+#'   \item{latitude}{latitude in decimal degrees, WGS84}
+#'   \item{longitude}{longitude in decimal degrees, WGS84}
+#'   \item{distance}{width of vertical band in nmi}
+#'  }
+#' @name horisontalProfile
+NULL
+
+#' Vertical profile
+#' @description
+#'  Makes vertical profile (sa by sailed distance)
+#' @details
+#'  Consult reference table for acoustic categories.
+#'  https://referenceeditor.hi.no/apps/referenceeditor/v2/tables/acousticCategory
+#'
+#'  some common options:
+#'  1 (other)
+#'  2
+#'  6
+#'  12 (herring)
+#'  22 (saithe)
+#'  30 (haddock)
+#'  31 (cod)
+#'
+#' @param echosounder luf20 as read by RstoxData::readXmlFile
+#' @param targetAcocat acoustic category to extract profile for
+#' @param channelType the channel type to extract profile from (e.g. Pelagic (P) or Bottom (B))
+#' @param freq frequncy of transreceiver to extract data from.
+#' @return \code{\link[acousticsQA]{verticalProfile}}
+#' @export
+verticalProfileLUF20 <- function(echosounder, targetAcocat, channelType="P", freq=38000){
+  target <- echosounder$sa_by_acocat[echosounder$sa_by_acocat$acocat==targetAcocat,]
+  targetPC <- target[target$type==channelType,]
+  targetPC38 <- targetPC[targetPC$freq==freq,]
+  targetSA <- merge(targetPC38, echosounder$sa, all.x=T, by=c("log_start", "start_time", "freq", "transceiver", "type", "acocat"))
+  stopifnot(length(unique(targetSA$acocat))==1)
+  stopifnot(length(unique(targetSA$freq))==1)
+  stopifnot(length(unique(targetSA$transceiver))==1)
+  stopifnot(length(unique(targetSA$type))==1)
+  targetSAInt <- targetSA[, list(sa=sum(get("sa"))), by=list(log_start=get("log_start"), start_time=get("start_time"))]
+  dist <- echosounder$distance[,c("log_start", "start_time", "lat_start", "lon_start", "integrator_dist"), with=F]
+  targetSAInt <- merge(targetSAInt, dist, all.y=T, by=c("log_start", "start_time"))
+  targetSAInt$start_time <- as.POSIXct(targetSAInt$start_time)
+  targetSAInt <- targetSAInt[order(targetSAInt$start_time),]
+  targetSAInt <- targetSAInt[,c("sa", "log_start", "start_time", "lat_start", "lon_start", "integrator_dist")]
+  names(targetSAInt) <- c("sa", "log", "time", "latitude", "longitude", "distance")
+
+
+  return(targetSAInt)
+}
+
+#' Trawl locations
+#' @description
+#'  Trawl locations
+#' @details
+#'  \code{\link[data.table]{data.table}} with columns
+#'  \describe{
+#'   \item{log}{log, sailed distance in nmi}
+#'   \item{time}{time as POSIXct}
+#'   \item{latitude}{latitude in decimal degrees, WGS84}
+#'   \item{longitude}{longitude in decimal degrees, WGS84}
+#'  }
+#'
+#' @name trawlLocation
+#'
+NULL
+
+#' Extract Trawl
+#' @description
+#'  Extract trawl locations from Biotic data
+#' @param biotic NMDbiotic data (v3.x) as parsed by RstoxData::readXmlFile
+#' @param serialnumbers serialnumbers to include. If NULL all are included. Compared to column (fishstation/serialnumber)
+#' @param sampleQuality sample quality codes to include. If NULL all are included. Compared to column (fishstation/samplequality)
+#' @param serialnumbers gear codes to include. If NULL all are included. Compared to column (fishstation/gear)
+#' @return \code{\link[acousticsQA]{trawlLocation}}
+#' @export
+extractTrawlsBiotic <- function(biotic, serialnumbers=NULL, sampleQuality=NULL, gearCodes=NULL){
+
+  st <- biotic$fishstation
+  if (!is.null(serialnumbers)){
+    st <- st[st$serialnumber %in% serialnumbers]
+  }
+  if (!is.null(sampleQuality)){
+    st <- st[st$samplequality %in% sampleQuality]
+  }
+  if (!is.null(gearCodes)){
+    st <- st[st$gear %in% gearCodes]
+  }
+
+  st$time <- as.POSIXct(paste(st$stationstartdate, st$stationstarttime))
+  st <- st[,c("logstart", "time", "latitudestart", "longitudestart")]
+  names(st) <- c("log", "time", "latitidue", "longitude")
+
+  return(st)
+}
+
+#' Plots horisontal profile
+#' @description
+#'  Plots aggregated sa binned by distance from bottom
+#' @details
+#'  Plots aggregated sa binned by distance from bottom
+#'  Cumulative fractions are plotted on an alternate y-axis
+#' @param profile \code{\link[acousticsQA]{horisontalProfile}}
+#' @param header header for plot
+#' @export
+plotHorisontalProfile <- function(profile, header=""){
+  profile$sa2 <- max(profile$sa)*profile$sa/sum(profile$sa)
+  coeff <- 1/sum(profile$sa2)
+  ggplot2::ggplot(profile, ggplot2::aes(x=height)) +
+    ggplot2::geom_col(ggplot2::aes(y=sa)) +
+    ggplot2::geom_point(ggplot2::aes(y=cumsum(sa2)), group=1) +
+    ggplot2::scale_y_continuous(
+
+      # Features of the first axis
+      name = "total sA",
+
+      # Add a second axis and specify its features
+      sec.axis = ggplot2::sec_axis(~.*coeff, name="cum Frac sA")
+    ) +
+    ggplot2::ggtitle(header) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+
+}
+
+#' Plot vertical profile
+#' @description
+#'  Plots aggregated sa for each vertical band
+#' @details
+#'  Plots aggregated sa for each vertical band
+#'  Cumulative fractions are plotted on an alternate y-axis
+#' @param profile \code{\link[acousticsQA]{verticalProfile}}
+#' @param header header for plot
+#' @export
+plotStretch <- function(profile, header=""){
+
+  profile$sa2 <- max(profile$sa)*profile$sa/sum(profile$sa)
+  coeff <- 1/sum(profile$sa2)
+  ggplot2::ggplot(profile) +
+    ggplot2::geom_col(ggplot2::aes_string(x="log", y="sa"), width = .99) +
+    ggplot2::geom_line(ggplot2::aes(x=log, y=cumsum(sa2))) +
+    ggplot2::scale_y_continuous(
+
+      # Features of the first axis
+      name = "total sA",
+
+      # Add a second axis and specify its features
+      sec.axis = ggplot2::sec_axis(~.*coeff, name="cum Frac sA")
+    ) +
+    ggplot2::ggtitle(header) +
+    ggplot2::theme_bw()
+}
+
+#' Plot concentration
+#' @description
+#'  Plots cumulative sa against cumulative distance,
+#'  in order of decreasing sa
+#' @param profile \code{\link[acousticsQA]{verticalProfile}}
+#' @param header header for plot
+#' @export
+plotConcentration <- function(profile, header=""){
+  profile$sa[is.na(profile$sa)] <- 0
+  profile <- profile[order(profile$sa, decreasing = T),]
+  profile$cumSum <- cumsum(profile$sa)
+  profile$cumFrac <- profile$cumSum / sum(profile$sa)
+  profile$distance <- cumsum(profile$distance)
+  ggplot2::ggplot(profile) +
+    ggplot2::geom_path(ggplot2::aes_string(x="distance", y="cumSum")) +
+    ggplot2::ylab("Cum. sa") +
+    ggplot2::xlab("Cum. distance (nmi)") +
+    ggplot2::ggtitle(header) +
+    ggplot2:: theme_bw()
+
+}
+
+#' Plot lognormal QQ plot
+#' @description
+#'  Plot quantiles of log-transformed sa against quantiles of normal distribution.
+#' @param profile \code{\link[acousticsQA]{verticalProfile}}, NAs are ignored.
+#' @param header header for plot
+#' @export
+plotLogNormalQQ <- function(profile, header=""){
+  profile <- profile[!is.na(profile$sa),]
+
+  profile$logsa <- log(profile$sa)
+  ggplot2::ggplot(profile, ggplot2::aes_string(sample="logsa")) +
+    ggplot2::stat_qq() +
+    ggplot2::geom_qq_line() +
+    ggplot2::ylab("ln sa quantiles") +
+    ggplot2::xlab("std normal quantiles") +
+    ggplot2::ggtitle(header) +
+    ggplot2::theme_bw()
+}
+
+#' Plot histogram of ln sa
+#' @description
+#'  Plot distribution of ln sa over vertical bands as histogram
+#' @param profile \code{\link[acousticsQA]{verticalProfile}}, NAs are ignored.
+#' @param header header for plot
+#' @export
+plotLogSaHistogram <- function(profile, header=""){
+  profile <- profile[!is.na(profile$sa),]
+  profile$logsa <- log(profile$sa)
+
+  ggplot2::ggplot(profile, ggplot2::aes_string(sample="logsa")) +
+    ggplot2::geom_histogram(ggplot2::aes_string(x="logsa"), bins = 30) +
+    ggplot2::xlab("ln sa") +
+    ggplot2::ggtitle(header) +
+    ggplot2::theme_bw()
+}
+
+#' @noRd
+plotDistanceTrawl <- function(profile, trawls, header=""){
+  profile$trawlDist <- as.numeric(NA)
+  for (i in 1:nrow(profile)){
+    minDist <- NA
+    for (j in 1:nrow(trawls)){
+      dist <- geosphere::distHaversine(c(profile$longitude[i], profile$latitude[i]), c(trawls$longitude[j], trawls$latitidue[j])) / 1852
+      minDist <- min(minDist, dist, na.rm=T)
+    }
+    profile$trawlDist[i] <- minDist
+  }
+  profile$sa[is.na(profile$sa)] <- 0
+  ggplot2::ggplot(profile) +
+    ggplot2::geom_point(ggplot2::aes_string(y="sa", x="trawlDist")) +
+    ggplot2::ggtitle(header) +
+    ggplot2::ylab("allocated sa") +
+    ggplot2::xlab("nearest trawl (nmi)") +
+    ggplot2::theme_bw()
+
+}
+
+# plot sa vs distance to trawl
+
+# add fraction of total fish in 'plotConcentration' as color ?
+
+# make variant with 2-d grid mot mercator proj coordinates and survey trace, support adding trawled positions
+
+
+
